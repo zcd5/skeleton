f6a234d107f1f16e3210b4cb50dea1b8dde5613f
Zahria Davis <zcd5@students.uwf.edu>
2024-10-02 14:14:47 -0500

    worked on cutness lab

--- app.js
+++ app.js
@@ -12,16 +12,41 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(express.urlencoded())
+
+const pets = [
+	{
+		name: "Scout",
+		image: "cute-dog.jpg",
+		superpower: "paws"
+	},
+	{
+		name: "Toby",
+		image: "cute-cat.jpg",
+		superpower: "eyes"
+	}
+] 
+
+const votes =[];
 
 app.get('/', function(req, res) {
-	res.render('home');
+  res.render('home', {pets});            //pets is both the key and the value
 });
+
+app.post("/vote", (req, res) =>{
+	console.log(req.body)
+	votes.push({...req.body, ip: req.ip});
+	console.log(votes)
+	res.redirect('/')
+  })
+  
 
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next){
 	res.status(404);
 	res.render('404');
-});
+}); 
+
 
 // 500 error handler (middleware)
 app.use(function(err, req, res, next){
